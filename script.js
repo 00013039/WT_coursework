ce2e4d2c6d0b9311e51baf847dbecc7c099bf824
00013039 <115616080+00013039@users.noreply.github.com>
2022-11-22 22:23:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const nav = document.getElementById('navbar')
+
+const bar = document.getElementById('bar')
+
+const close = document.getElementById('close')
+
+if (bar)
+{
+    bar.addEventListener('click', ()=> 
+    {
+        nav.classList.add('active');
+    }
+    )
+}
+
+if (close)
+{
+    bar.addEventListener('click', ()=> 
+    {
+        nav.classList.remove('active');
+    }
+    )
+}
